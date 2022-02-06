971de8e00bf461177c074b46f14844b67726b840
Chidera Mosanya <deramosanya@gmail.com>
2022-02-06 03:13:12 +0100

    init project structure

---
+++ Hadoop Project/src/main/java/com/roseline/hadoop/jobs/TransactionExaminerJob.java
@@ -0,0 +1,15 @@
+package com.roseline.hadoop.jobs;
+
+import org.apache.hadoop.hdfs.client.HdfsUtils;
+
+public class TransactionExaminerJob {
+    String engineName;
+
+    public TransactionExaminerJob(String engineName){
+        this.engineName = engineName;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
